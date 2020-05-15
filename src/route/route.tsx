acdae402f2541e35719aa7d1ef4e07bf43e08ae8
David Sanders <dsanders_m633@outlook.com>
2020-05-15 11:47:00 -0400

    removed comment

--- src/route/route.tsx
+++ src/route/route.tsx
@@ -6,9 +6,6 @@ import React from 'react';
 import {Route as ReactRouterRoute} from 'react-router-dom';
 
 export default function Route(props: any){
-    // React.useEffect(() => {
-    //     console.log(props.children);
-    // },[])
     return(
         <>
             <ReactRouterRoute {...props}/>
